2d6f11ecc31b2967945672d7ff62711cfa3edb8b
gsosa2 <gsosa2@ggc.edu>
2025-11-21 10:18:47 -0500

    Updating Project Name

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "CodeFolio",
   description: "Created with v0",
-  generator: "v0.app",
+  generator: "v0.app.",
 }
 
 export default function RootLayout({
